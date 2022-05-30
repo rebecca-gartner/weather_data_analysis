930d46cc97ec37f138702420f127ac2c3fe5ddaf
the unicorn <ty226@stud.uni-heidelberg.de>
2022-05-30 12:52:27 +0200

    review kritik verbessert

--- database_mapper/mapper.py
+++ database_mapper/mapper.py
@@ -3,14 +3,16 @@ from asyncio.log import logger
 from datetime import datetime
 from pymongo import MongoClient
 import pytz
+import os
 
 import psycopg2
 from config import config
 
-date = 2022
+DATE = 2022
+PATH = "database_mapper"
 
 
-def main(Date):
+def main(date):
     """
     extracts weather data from MongoDB and inserts it into a PostreSQL table
 
@@ -18,103 +20,115 @@ def main(Date):
     date: date for which weather data exists in MongoDB
     """
     # connect to MongoDB and create Weather DB
-    client = MongoClient(port=27017)
+    client = MongoClient(port=os.environ.get("PORT"))
     db = client.weather_data
     col = db.reviews
 
     params = config()
 
-    conn = psycopg2.connect(**params)
-    conn.autocommit = True
-    cursor = conn.cursor()
+    with psycopg2.connect(**params) as conn:
+        with conn.cursor() as cursor:
+            conn.autocommit = True
 
-    # creates a PostgreSQL table that contains the weather data
-    with open(
-        "database_mapper\create_weather_table.sql",
-        "r",
-    ) as sql_file:
-        cursor.execute(sql_file.read())
+            # creates a PostgreSQL table that contains the weather data
+            with open(os.path.join(PATH, "create_weather_table.sql"), "r") as sql_file:
+                try:
+                    cursor.execute(sql_file.read())
+                except psycopg2.errors.SqlStatementNotYetComplete:
+                    print("SQL statement incomplete")
+                except psycopg2.errors.ConnectionException:
+                    print("a connection problem occured")
 
-    # creates a PostgreSQL table that contains the sources of the weather data
-    with open(
-        "database_mapper\create_source_table.sql",
-        "r",
-    ) as sql_file:
-        cursor.execute(sql_file.read())
+            # creates a PostgreSQL table that contains the sources of the weather data
+            with open(os.path.join(PATH, "create_source_table.sql"), "r") as sql_file:
+                try:
+                    cursor.execute(sql_file.read())
+                except psycopg2.errors.SqlStatementNotYetComplete:
+                    print("SQL statement incomplete")
+                except psycopg2.errors.ConnectionException:
+                    print("a connection problem occured")
 
-    # in the source table every source should only be listed once.
-    # Therefore the distinct source ids are  put in the id_list
-    with open(
-        "database_mapper\check_source_id.sql",
-        "r",
-    ) as sql_file:
-        cursor.execute(sql_file.read())
+            # in the source table every source should only be listed once.
+            # Therefore the distinct source ids are  put in the id_list
+            with open(os.path.join(PATH, "check_source_id.sql"), "r") as sql_file:
+                try:
+                    cursor.execute(sql_file.read())
+                except psycopg2.errors.SqlStatementNotYetComplete:
+                    print("SQL statement incomplete")
+                except psycopg2.errors.ConnectionException:
+                    print("a connection problem occured")
 
-    ids = cursor.fetchall()
-    id_list = []
-    if len(ids) > 0:
-        for i in range(len(ids[0])):
-            id_list.append(ids[0][i])
+            ids = cursor.fetchall()
+            id_list = []
+            if len(ids) > 0:
+                for i in range(len(ids[0])):
+                    id_list.append(ids[0][i])
 
-    # all weather data that has the given timestamp in
-    # its collection is added to the weather_list
-    weather_list = []
-    try:
-        x = col.find_one(
-            {"data.weather.timestamp": {"$regex": f".*{date}.*"}}
-        )  # in UTC time
-    except:
-        logger.info("no matching entry found in MongoDB")
+            # all weather data that has the given timestamp in
+            # its collection is added to the weather_list
+            weather_list = []
+            try:
+                x = col.find_one(
+                    {"data.weather.timestamp": {"$regex": f".*{date}.*"}}
+                )  # in UTC time
+                weather_list.append(x)
+                columns = list(weather_list[0]["data"]["weather"][0].keys())
+                columns = "[{}]".format(", ".join(columns))
 
-    weather_list.append(x)
+                # the weather data gets extracted from the weather_list (which
+                # also contains some unneccessary data)
+                for weather_entry in weather_list[0]["data"]["weather"]:
+                    weather_values_list = list(weather_entry.values())
 
-    columns = list(weather_list[0]["data"]["weather"][0].keys())
+                    # changes timezone to European time
+                    weather_values_list[0] = datetime.strptime(
+                        weather_values_list[0], "%Y-%m-%dT%H:%M:%S%z"
+                    )
+                    weather_values_list[0] = weather_values_list[0].astimezone(
+                        pytz.timezone("Europe/Berlin")
+                    )
 
-    columns = "[{}]".format(", ".join(columns))
+                    # the BrightSky API also gives us weather information from
+                    # the fallback source id. However, this is usually not
+                    # relevant and therefore all the information is put into
+                    # a string and added as one column to the weather table
+                    weather_values_list[15] = str(weather_values_list[15])
 
-    # the weather data gets extracted from the weather_list (which
-    # also contains some unneccessary data)
-    for j in range(len(weather_list[0]["data"]["weather"])):
+                    # weather data gets inserted in weather table
+                    weather_values_tuple = tuple(weather_values_list)
+                    with open(
+                        os.path.join(PATH, "insert_into_weather_table.sql"), "r"
+                    ) as sql_file:
+                        try:
+                            cursor.execute(sql_file.read(), weather_values_tuple)
+                        except psycopg2.errors.SqlStatementNotYetComplete:
+                            print("SQL statement incomplete")
+                        except psycopg2.errors.ConnectionException:
+                            print("a connection problem occured")
+                        except psycopg2.errors.DatabaseError:
+                            print("error inserting the data")
 
-        weather_values_list = list(weather_list[0]["data"]["weather"][j].values())
-        # changes timezone to European time
-        weather_values_list[0] = datetime.strptime(
-            weather_values_list[0], "%Y-%m-%dT%H:%M:%S%z"
-        )
-        weather_values_list[0] = weather_values_list[0].astimezone(
-            pytz.timezone("Europe/Berlin")
-        )
+                # all distinct sources get inserted in the source table
+                for weather_source_entry in weather_list[0]["data"]["sources"]:
+                    weather_source_values_list = list(weather_source_entry.values())
+                    weather_source_tuple = tuple(weather_source_values_list)
 
-        # the BrightSky API also gives us weather information from
-        # the fallback source id. However, this is usually not
-        # relevant and therefore all the information is put into
-        # a string and added as one column to the weather table
-        weather_values_list[15] = str(weather_values_list[15])
+                    if weather_source_tuple[0] not in id_list:
+                        with open(
+                            os.path.join(PATH, "insert_into_source_table.sql"), "r"
+                        ) as sql_file:
+                            try:
+                                cursor.execute(sql_file.read(), weather_source_tuple)
+                            except psycopg2.errors.SqlStatementNotYetComplete:
+                                print("SQL statement incomplete")
+                            except psycopg2.errors.ConnectionException:
+                                print("a connection problem occured")
+                            except psycopg2.errors.DatabaseError:
+                                print("error inserting the data")
 
-        # weather data gets inserted in weather table
-        weather_values_tuple = tuple(weather_values_list)
-        with open(
-            "database_mapper\insert_into_weather_table.sql",
-            "r",
-        ) as sql_file:
-            cursor.execute(sql_file.read(), weather_values_tuple)
-
-    # all distinct sources get inserted in the source table
-    for j in range(len(weather_list[0]["data"]["sources"])):
-        weather_source_tuple = tuple(
-            list(weather_list[0]["data"]["sources"][j].values())
-        )
-
-        if weather_source_tuple[0] not in id_list:
-            with open(
-                "database_mapper\insert_into_source_table.sql",
-                "r",
-            ) as sql_file:
-                cursor.execute(sql_file.read(), weather_source_tuple)
-
-    conn.commit()
-    conn.close()
+            except:
+                logger.error("no matching entry found in MongoDB")
 
 
 if __name__ == "__main__":
-    main(date)
+    main(DATE)
